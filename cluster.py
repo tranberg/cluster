975733e3cf525eac47238ff05a745fd70ae953ef
Bo Tranberg <bo@tberg.dk>
2015-07-08 13:23:47 +0200

    cleaning

--- cluster.py
+++ cluster.py
@@ -12,7 +12,7 @@ from routines import *  # see routines.py
 # 1.5 hours to complete with 100 realisations and 1e6 nodes. It also saves
 # average node degrees for the largest network averaged over all runs.
 
-runs = 1000   # Number of realizations
+runs = 1000  # Number of realizations
 
 # Here we build a network, calculate the clustering coefficients and the node
 # degrees. This is repeated "runs" times for each of the network sizes specified
@@ -25,77 +25,79 @@ kk = []
 TTC = []
 last = False
 lastRun = False
-for N in [10,100,1000,10000,100000]: # network sizes
+for N in [10, 100, 1000, 10000, 100000]:  # network sizes
     if N < 100000:
-        top = N*10
+        top = N * 10
     else:
-        top = N*10+3*N
+        top = N * 10 + 3 * N
         last = True
 
-    for n in range(N,top,N):
+    for n in range(N, top, N):
         if last:
-            if n == top-N:
+            if n == top - N:
                 lastRun = True
         tc = []
-        if n == 10: line = ''
-        else: line = '\n'
-        print line+'Building network with',n,'nodes.'
+        if n == 10:
+            line = ''
+        else:
+            line = '\n'
+        print line + 'Building network with', n, 'nodes.'
         for k in range(runs):
-            print "\r",'Run',k+1,'of',str(runs),
+            print "\r", 'Run', k + 1, 'of', str(runs),
             sys.stdout.flush()
-    
+
             # Initial network
-            irow = [0,0,1]
-            icol = [1,2,2]
-    
+            irow = [0, 0, 1]
+            icol = [1, 2, 2]
+
             # Adding nodes
-            row,col,data=[],[],[]
-            row,col = add(irow,icol,n)
+            row, col, data = [], [], []
+            row, col = add(irow, icol, n)
             data = where(row)
-    
+
             # Building the complete adjacency matrix
-            j = int(2*(n+2)-1)
-            nodes = 3+(j-3)/2
-            tdata = np.concatenate((data[:j],data[:j]))
-            trow = np.concatenate((row[:j],col[:j]))
-            tcol = np.concatenate((col[:j],row[:j]))
-            A = csr_matrix((tdata,([trow,tcol])))
-            
+            j = int(2 * (n + 2) - 1)
+            nodes = 3 + (j - 3) / 2
+            tdata = np.concatenate((data[:j], data[:j]))
+            trow = np.concatenate((row[:j], col[:j]))
+            tcol = np.concatenate((col[:j], row[:j]))
+            A = csr_matrix((tdata, ([trow, tcol])))
+
             # Calculating node degrees and clustering coefficients.
             C = []
-            AAA = A*A               # Matrix multiplication split in two in order
-            C = AAA*A               # to save memory.
+            AA = A * A             # Matrix multiplication split in two in order
+            C = AA * A             # to save memory.
             C = C.diagonal()
-    
+
             k = A.sum(0)            # Calculating node degrees.
             if lastRun:             # Saving node degrees for the largest network.
                 kk.append(k)
-            f = np.multiply(k,(k-1))
-            W = C/f
-            WW = W.sum()/nodes      # Total clustering coefficient for the network.
+            f = np.multiply(k, (k - 1))
+            W = C / f
+            WW = W.sum() / nodes    # Total clustering coefficient for the network.
             tc.append(WW)
-        
+
         mins.append(np.min(tc))
         maxs.append(np.max(tc))
         means.append(np.mean(tc))
-    
+
         TTC.append(tc)
 
-mins = np.abs(np.subtract(mins,means))
-maxs = np.subtract(maxs,means)
+mins = np.abs(np.subtract(mins, means))
+maxs = np.subtract(maxs, means)
 
 # Averaging over all realisations in order to obtain an average degree
 # distribution.
 top = np.max(kk)
-H = np.zeros((len(kk),top))
-bins = np.linspace(0,top+1,top+1)
+H = np.zeros((len(kk), top))
+bins = np.linspace(0, top + 1, top + 1)
 for i in range(len(kk)):
-    H[i],bins = np.histogram(kk[i],bins)
-k = np.mean(H,0)
+    H[i], bins = np.histogram(kk[i], bins)
+k = np.mean(H, 0)
 
 # Save data to file for later plotting.
-np.savetxt('results_'+str(runs)+'_realisations',[mins,maxs,means])
-np.savez('avk',k=k)
-np.save('TTC.npy',TTC)
+np.savetxt('results_' + str(runs) + '_realisations', [mins, maxs, means])
+np.savez('avk', k=k)
+np.save('TTC' + str(runs) + '.npy', TTC)
 print "Done!"
-print "Calculations took "+str((time()-start)/3600)+" hours."
+print "Calculations took " + str((time() - start) / 3600) + " hours."
--- scatterplotter.py
+++ scatterplotter.py
@@ -1,40 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-C = np.load('TTC.npy')
+C = np.load('TTC_1000.npy')
+runs = 1000
 
 x = []
 for N in [10, 100, 1000, 10000, 100000]:
     if N < 100000:
-        top = N*10
+        top = N * 10
     else:
-        top = N*10+3*N
-    for n in range(N,top,N):
+        top = N * 10 + 3 * N
+    for n in range(N, top, N):
         x.append(n)
 
-means = np.mean(C,1)
-mins = np.min(C,1)
-maxs = np.max(C,1)
-stds = np.std(C,1)
+means = np.mean(C, 1)
+mins = np.min(C, 1)
+maxs = np.max(C, 1)
+stds = np.std(C, 1)
 
 print 'Mean: ', "%.5f" % means[-1]
-print 'Plus: ', "%.5f" % (maxs[-1]-means[-1])
-print 'Minus: ', "%.5f" % (means[-1]-mins[-1])
+print 'Plus: ', "%.5f" % (maxs[-1] - means[-1])
+print 'Minus: ', "%.5f" % (means[-1] - mins[-1])
 
 meanplot, = plt.plot(x, means, '-', lw=1.5, color="#000099")
 minplot, = plt.plot(x, mins, '-k', lw=1, alpha=0.2)
 plt.plot(x, maxs, '-k', lw=1.2, alpha=0.2)
-stdplot, = plt.plot(x, means+stds, ':k', lw=1.5, alpha=.6)
-plt.plot(x, means-stds, ':k', lw=1.5, alpha=.6)
+stdplot, = plt.plot(x, means + stds, ':k', lw=1.5, alpha=.6)
+plt.plot(x, means - stds, ':k', lw=1.5, alpha=.6)
 
 for r in range(48):
-    plt.plot([x[r] for i in range(1000)],C[r],'.k', alpha=.02)
-scatter, = plt.plot([-10 for i in range(1000)],C[r],'.k')
+    plt.plot([x[r] for i in range(runs)], C[r], '.k', alpha=.02)
+scatter, = plt.plot([-10 for i in range(runs)], C[r], '.k')
 
-plt.legend([meanplot, minplot, stdplot, scatter],['Mean C', 'Max/min C', r'$\sigma$', 'Simulations'], loc=1)
+plt.legend([meanplot, minplot, stdplot, scatter],
+           ['Mean C', 'Max/min C', r'$\sigma$', 'Simulations'], loc=1)
 
 plt.xscale('log')
-plt.axis([9 ,1.1*x[-1], min(mins), max(maxs)])
+plt.axis([9, 1.1 * x[-1], min(mins), max(maxs)])
 plt.xlabel('Number of nodes in network')
 plt.ylabel('Clustering coefficient, C')
-plt.savefig('scatter.png',bbox_inches='tight')
+plt.savefig('scatter_' + str(runs) + '.png', bbox_inches='tight')
